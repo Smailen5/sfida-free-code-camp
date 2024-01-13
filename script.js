8f6a3eebfb60d456866d1c5b2ca6eb66f525f495
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-01-13 13:53:39 +0100

    ottimizazione del form

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 
 console.log('Script caricato con successo!');
+alert(`Ciao, una volta inseriti i dati all'interno del form puoi vederli stampati in console,
+per utilizzare gli strumenti da sviluppatori premi F12 o click destro e seleziona ispeziona`)
 
 function handleSubmit(event) {
     console.log('Funzione handleSubmit chiamata!');
